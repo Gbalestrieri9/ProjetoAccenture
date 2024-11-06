a151f6b948ee43ae4e86233a48a1291e93533c06
Gbalestrieri9 <gabrielbalestrieri@hotmail.com>
2024-11-06 15:37:55 -0300

    git innitial

---
+++ src/main/java/com/projetoAccenture/ProjetoAccentureApplication.java
@@ -0,0 +1,13 @@
+package com.projetoAccenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAccentureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoAccenture/ProjetoAccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoAccenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
